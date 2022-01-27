cb3f11f35141716f0dc4a269b8a2fcbe4370a21a
dwestland <don@westland.net>
2022-01-26 21:36:23 -0800

    Update upload API

--- pages/api/upload/index.ts
+++ pages/api/upload/index.ts
@@ -1,7 +1,7 @@
 import { IncomingForm } from 'formidable'
-import { promises as fs } from 'fs'
+import mv from 'mv'
 
-const mv = require('mv')
+// const mv = require('mv')
 
 export const config = {
   api: {
@@ -10,17 +10,18 @@ export const config = {
 }
 
 export default async (req, res) => {
-  const data = await new Promise((resolve, reject) => {
+  await new Promise((resolve, reject) => {
     const form = new IncomingForm()
 
     form.parse(req, (err, fields, files) => {
       if (err) return reject(err)
-      console.log('boom', fields, files)
-      console.log('boom2', files.file.filepath)
       const oldPath = files.file.filepath
       const newPath = `./public/images/uploads/${files.file.originalFilename}`
+
       mv(oldPath, newPath, (err) => {})
       res.status(200).json({ fields, files })
+
+      return null
     })
   })
 }
